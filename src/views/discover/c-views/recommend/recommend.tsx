1a01c992a5ed093f4983a5727b950f3dd24d9350
sakura <11854578@qq.com>
2023-04-01 14:39:21 +0800

    axios的网络请求封装和测试

--- src/views/discover/c-views/recommend/recommend.tsx
+++ src/views/discover/c-views/recommend/recommend.tsx
@@ -1,12 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import { request } from '@/utils/request'
+import type { Banner, Banners } from '@/types/recommend'
 
 interface IProps {
   children?: ReactNode
 }
 
 const Recommend: FC<IProps> = () => {
-  return <div>Recommend</div>
+  const [banners, setBanners] = useState<Banner[]>([])
+  useEffect(() => {
+    request<Banners>('/banner').then((res) => {
+      setBanners(res.data.banners)
+    })
+  }, [])
+  return (
+    <div>
+      {banners.map((item) => {
+        return <div key={item.imageUrl}>{item.imageUrl}</div>
+      })}
+    </div>
+  )
 }
 
 export default memo(Recommend)
